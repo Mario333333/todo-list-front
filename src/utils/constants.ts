171bd98c542a0895d492346ed4915352a95f52b3
Mario333333 <mariocgc@icloud.com>
2024-07-29 14:43:51 -0600

    setting api to production

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
 export const API_URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:4001/v1/api/";
+  process.env.NODE_ENV === "production"
+    ? "https://tasks-restfull-api.vercel.app/v1/api/"
+    : "http://localhost:4001/v1/api/";
 
-  export const CONTENT_TYPE = "Content-Type";
-  export const CONTENT_TYPE_VALUE = "application/json";
+export const CONTENT_TYPE = "Content-Type";
+export const CONTENT_TYPE_VALUE = "application/json";
